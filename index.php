b173d76a2be8f68e2bd2e28e4af904474edd9c80
basiaszafraniec <basia.szafraniec@gmail.com>
2022-10-23 10:53:01 +0200

    suwmania -> suwmaniatest

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Suwmania</title>
+    <title>Suwmaniatest</title>
     <link rel="icon" type="image/x-icon" href="faviconPink.png">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
